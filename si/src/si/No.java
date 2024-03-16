e53b19af6c1e2fcd464faa59a234e1523602f688
BL4Z3-Sebas <158209851+BL4Z3-Sebas@users.noreply.github.com>
2024-03-15 23:18:23 -0500

    daleeee

---
+++ si/src/si/No.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package si;
+
+
+public class No {
+    private String lol;
+
+    public No(String lol) {
+        this.lol = lol;
+    }
+
+    public String getLol() {
+        return lol;
+    }
+
+    public void setLol(String lol) {
+        this.lol = lol;
+    }
+    
+}
